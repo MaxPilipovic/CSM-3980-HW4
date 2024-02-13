d8b6f0f41500514f6855c86b59dc59a852b4de3d
MaxPilipovic <maxpkusa@gmail.com>
2024-02-12 17:38:56 -0800

    - Sequential

--- Sequential.java
+++ Sequential.java
@@ -1,7 +1,7 @@
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Sequential {
-    //private static final AtomicInteger test = new AtomicInteger(0); //Atomic Integer
+    private static final AtomicInteger test = new AtomicInteger(0); //Atomic Integer
     public static void main(String[] args) {
         int i = 10;
         final Stopwatch watch = new Stopwatch();
@@ -14,15 +14,15 @@ public class Sequential {
                         if (check >= 0) {
                             int squareRoot = (int) Math.sqrt(check);
                             if (squareRoot * squareRoot == check) {
-                                //test.incrementAndGet();
-                                count++;
+                                test.incrementAndGet();
+                                //count++;
                             }
                         }
                     }
                 }
             }
         }
-        System.out.println("Sequential finished and value is " + count);
+        System.out.println("Sequential finished and value is " + test.get());
         System.out.printf("Program took %f seconds\n", watch.elapsedTime());
         System.out.println("");
     }
